310adc80cf590090b025cbcbd17aa779a77b753b
phoenixsampras <phoenixsampras@gmail.com>
2020-08-25 10:56:59 -0400

    Ventas 3.0

--- sh_purchase_price_history/models/purchase_price_history.py
+++ sh_purchase_price_history/models/purchase_price_history.py
@@ -40,48 +40,49 @@ class ProductTemplate(models.Model):
     purchase_price_history_line_ids = fields.Many2many("purchase.price.history", string="Price History Lines", compute="get_product_supplier_price")        
  
     def get_product_supplier_price(self):
-        if self and self.id:
-            if self.env.user.company_id:
+        for res in self:
+            if res and res.id:
+                if self.env.user.company_id:
+                    
+                    cond = self.env.user.company_id.record_based_on_purchase
+                    itm_limit = self.env.user.company_id.item_limit_purchase
+                    purchase_price_line = []    
                 
-                cond = self.env.user.company_id.record_based_on_purchase
-                itm_limit = self.env.user.company_id.item_limit_purchase
-                purchase_price_line = []    
-             
-                if cond == 'both':
-                    purchase_line_obj = self.env['purchase.order.line'].sudo().search([('product_id', 'in', self.product_variant_ids.ids), ('state', 'in', ('purchase', 'done'))], limit=itm_limit, order='create_date desc')
-                else :
-                    purchase_line_obj = self.env['purchase.order.line'].sudo().search([('product_id', 'in', self.product_variant_ids.ids), ('state', '=', str(cond))], limit=itm_limit, order='create_date desc')
-                
-                if purchase_line_obj:
-                    for record in purchase_line_obj:
-                        
-                        vals = {}
-                        vals.update({'name':record.id})
-                         
-                        if record.partner_id:
-                            vals.update({'partner_id':record.partner_id.id})
-     
-                        if record.product_id:    
-                            vals.update({'variant_id':record.product_id.id})
-                        if record.order_id:    
-                            vals.update({'purchase_order_id':record.order_id.id})
-                             
-                        if record.order_id.date_order :
-                            vals.update({'order_date':record.order_id.date_order})   
-                        if record.product_qty:
-                            vals.update({'quantity':record.product_qty})
-                        if record.price_unit: 
-                            vals.update({'purchase_price':record.price_unit})        
-                        if record.price_subtotal: 
-                            vals.update({'total_price':record.price_subtotal})
-                                 
-                        if vals:
-                            purchase_price_obj = self.env['purchase.price.history'].create(vals)
-                              
-                            if purchase_price_obj:
-                                purchase_price_line.append(purchase_price_obj.id)
-                
-                self.purchase_price_history_line_ids = purchase_price_line
+                    if cond == 'both':
+                        purchase_line_obj = self.env['purchase.order.line'].sudo().search([('product_id', 'in', res.product_variant_ids.ids), ('state', 'in', ('purchase', 'done'))], limit=itm_limit, order='create_date desc')
+                    else :
+                        purchase_line_obj = self.env['purchase.order.line'].sudo().search([('product_id', 'in', res.product_variant_ids.ids), ('state', '=', str(cond))], limit=itm_limit, order='create_date desc')
+                    
+                    if purchase_line_obj:
+                        for record in purchase_line_obj:
+                            
+                            vals = {}
+                            vals.update({'name':record.id})
+                            
+                            if record.partner_id:
+                                vals.update({'partner_id':record.partner_id.id})
+        
+                            if record.product_id:    
+                                vals.update({'variant_id':record.product_id.id})
+                            if record.order_id:    
+                                vals.update({'purchase_order_id':record.order_id.id})
+                                
+                            if record.order_id.date_order :
+                                vals.update({'order_date':record.order_id.date_order})   
+                            if record.product_qty:
+                                vals.update({'quantity':record.product_qty})
+                            if record.price_unit: 
+                                vals.update({'purchase_price':record.price_unit})        
+                            if record.price_subtotal: 
+                                vals.update({'total_price':record.price_subtotal})
+                                    
+                            if vals:
+                                purchase_price_obj = self.env['purchase.price.history'].create(vals)
+                                
+                                if purchase_price_obj:
+                                    purchase_price_line.append(purchase_price_obj.id)
+                    
+                    res.purchase_price_history_line_ids = purchase_price_line
                 
                 
 class ProductProduct(models.Model):
@@ -90,45 +91,46 @@ class ProductProduct(models.Model):
     purchase_price_history_line_ids = fields.Many2many("purchase.price.history", string="Price History Lines", compute="get_product_supplier_price")        
  
     def get_product_supplier_price(self):
-        if self and self.id:
-            if self.env.user.company_id:
+        for res in self:
+            if res and res.id:
+                if self.env.user.company_id:
+                    
+                    cond = self.env.user.company_id.record_based_on_purchase
+                    itm_limit = self.env.user.company_id.item_limit_purchase
+                    purchase_price_line = []    
                 
-                cond = self.env.user.company_id.record_based_on_purchase
-                itm_limit = self.env.user.company_id.item_limit_purchase
-                purchase_price_line = []    
-             
-                if cond == 'both':
-                    purchase_line_obj = self.env['purchase.order.line'].sudo().search([('product_id', 'in', [self.id]), ('state', 'in', ('purchase', 'done'))], limit=itm_limit, order='create_date desc')
-                else :
-                    purchase_line_obj = self.env['purchase.order.line'].sudo().search([('product_id', 'in', [self.id]), ('state', '=', str(cond))], limit=itm_limit, order='create_date desc')
-                
-                if purchase_line_obj:
-                    for record in purchase_line_obj:
-                        
-                        vals = {}
-                        vals.update({'name':record.id})
-                         
-                        if record.partner_id:
-                            vals.update({'partner_id':record.partner_id.id})
-     
-                        if record.product_id:    
-                            vals.update({'variant_id':record.product_id.id})
-                        if record.order_id:    
-                            vals.update({'purchase_order_id':record.order_id.id})
-                             
-                        if record.order_id.date_order :
-                            vals.update({'order_date':record.order_id.date_order})   
-                        if record.product_qty:
-                            vals.update({'quantity':record.product_qty})
-                        if record.price_unit: 
-                            vals.update({'purchase_price':record.price_unit})        
-                        if record.price_subtotal: 
-                            vals.update({'total_price':record.price_subtotal})
-                                 
-                        if vals:
-                            purchase_price_obj = self.env['purchase.price.history'].create(vals)
-                              
-                            if purchase_price_obj:
-                                purchase_price_line.append(purchase_price_obj.id)
-                
-                self.purchase_price_history_line_ids = purchase_price_line                
+                    if cond == 'both':
+                        purchase_line_obj = self.env['purchase.order.line'].sudo().search([('product_id', 'in', [res.id]), ('state', 'in', ('purchase', 'done'))], limit=itm_limit, order='create_date desc')
+                    else :
+                        purchase_line_obj = self.env['purchase.order.line'].sudo().search([('product_id', 'in', [res.id]), ('state', '=', str(cond))], limit=itm_limit, order='create_date desc')
+                    
+                    if purchase_line_obj:
+                        for record in purchase_line_obj:
+                            
+                            vals = {}
+                            vals.update({'name':record.id})
+                            
+                            if record.partner_id:
+                                vals.update({'partner_id':record.partner_id.id})
+        
+                            if record.product_id:    
+                                vals.update({'variant_id':record.product_id.id})
+                            if record.order_id:    
+                                vals.update({'purchase_order_id':record.order_id.id})
+                                
+                            if record.order_id.date_order :
+                                vals.update({'order_date':record.order_id.date_order})   
+                            if record.product_qty:
+                                vals.update({'quantity':record.product_qty})
+                            if record.price_unit: 
+                                vals.update({'purchase_price':record.price_unit})        
+                            if record.price_subtotal: 
+                                vals.update({'total_price':record.price_subtotal})
+                                    
+                            if vals:
+                                purchase_price_obj = self.env['purchase.price.history'].create(vals)
+                                
+                                if purchase_price_obj:
+                                    purchase_price_line.append(purchase_price_obj.id)
+                    
+                    res.purchase_price_history_line_ids = purchase_price_line                
